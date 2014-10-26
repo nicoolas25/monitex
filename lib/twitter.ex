046a1ba3ec76a836b5d03d96396d87e1c0fa7d16
Nicolas ZERMATI <nicoolas25@gmail.com>
2014-10-26 16:02:57 +0100

    Use a gen_server.

--- lib/twitter.ex
+++ lib/twitter.ex
@@ -4,39 +4,89 @@ defmodule Twitter do
   @authorize_url 'https://twitter.com/oauth/authorize'
   @prefix 'https://api.twitter.com/1.1'
 
-  def request_token(key, secret) do
+  use GenServer
+
+  # API
+
+  def create(key, secret) do
+    {:ok, pid} = GenServer.start(__MODULE__, {key, secret}, [])
+    pid
+  end
+
+  def get_request_tokens(client) do
+    GenServer.call(client, :get_request_tokens)
+  end
+
+  def authorize_url(client) do
+    GenServer.call(client, :authorize_url)
+  end
+
+  def get_access_tokens(client, verifier) do
+    GenServer.call(client, {:get_access_tokens, verifier})
+  end
+
+  def set_access_tokens(client, token, token_secret) do
+    GenServer.call(client, {:set_access_tokens, token, token_secret})
+  end
+
+  def set_request_tokens(client, token, token_secret) do
+    GenServer.call(client, {:set_request_tokens, token, token_secret})
+  end
+
+  def get(client, path, params \\ []) do
+    GenServer.call(client, {:get, path, params})
+  end
+
+  # Callbacks
+
+  def init({key, secret}) do
     consumer = {key, secret, :hmac_sha1}
-    {:ok, response} = :oauth.get(@request_token_url, [], consumer)
+    state = %{consumer: consumer, request_tokens: nil, tokens: nil}
+    {:ok, state}
+  end
+
+  def handle_call(:get_request_tokens, _from, state) do
+    {:ok, response} = :oauth.get(@request_token_url, [], state.consumer)
     request_token_params = :oauth.params_decode(response)
     request_token = :oauth.token(request_token_params)
     request_token_secret = :oauth.token_secret(request_token_params)
-    request_tokens = {request_token, request_token_secret}
-    {consumer, request_tokens}
+    next_state = %{state | request_tokens: {request_token, request_token_secret}}
+    {:reply, {:ok, next_state.request_tokens}, next_state}
   end
 
-  def authorize_url(_request_tokens = {request_token, _request_token_secret}) do
-    :oauth.uri(@authorize_url, [{'oauth_token', request_token}])
+  def handle_call(:authorize_url, _from, state) do
+    {token, _token_secret} = state.request_tokens
+    uri = :oauth.uri(@authorize_url, [{'oauth_token', token}])
+    {:reply, {:ok, uri}, state}
   end
 
-  def client_access_token(consumer, request_tokens, verifier) do
+  def handle_call({:get_access_tokens, verifier}, _from, state) do
+    {token, token_secret} = state.request_tokens
     params = [{'oauth_verifier', verifier}]
-    access_token(consumer, request_tokens, params)
-  end
-
-  # Only use with json.
-  def get(path, params, consumer, _access_tokens = {access_token, access_token_secret}) do
-    url = @prefix ++ path
-    {:ok, response} = :oauth.get(url, params, consumer, access_token, access_token_secret, [body_format: :binary])
-    {{'HTTP/1.1', 200, 'OK'}, _headers, body} = response
-    {:ok, decoded_body} = JSEX.decode(body)
-    decoded_body
-  end
-
-  defp access_token(consumer, _request_tokens = {request_token, request_token_secret}, params \\ []) do
-    {:ok, response} = :oauth.get(@access_token_url, params, consumer, request_token, request_token_secret)
+    {:ok, response} = :oauth.get(@access_token_url, params, state.consumer, token, token_secret)
     access_token_params = :oauth.params_decode(response)
     access_token = :oauth.token(access_token_params)
     access_token_secret = :oauth.token_secret(access_token_params)
-    {access_token, access_token_secret}
+    next_state = %{state | tokens: {access_token, access_token_secret}}
+    {:reply, {:ok, next_state.tokens}, next_state}
+  end
+
+  def handle_call({:set_access_tokens, token, token_secret}, _from, state) do
+    next_state = %{state | tokens: {token, token_secret}}
+    {:reply, {:ok, next_state.tokens}, next_state}
+  end
+
+  def handle_call({:set_request_tokens, token, token_secret}, _from, state) do
+    next_state = %{state | request_tokens: {token, token_secret}}
+    {:reply, {:ok, next_state.request_tokens}, next_state}
+  end
+
+  def handle_call({:get, path, params}, _from, state) do
+    url = @prefix ++ path
+    {token, token_secret} = state.tokens
+    {:ok, response} = :oauth.get(url, params, state.consumer, token, token_secret, [body_format: :binary])
+    {{'HTTP/1.1', 200, 'OK'}, _headers, body} = response
+    reply = JSEX.decode(body)
+    {:reply, reply, state}
   end
 end
--- test/twitter_test.exs
+++ test/twitter_test.exs
@@ -7,24 +7,30 @@ defmodule TwitterTest do
     ExVCR.Config.cassette_library_dir("test/fixture/cassettes")
   end
 
-  test "#request_token/2 returns consumer's tokens from key & secret" do
+  setup do
+    client = Twitter.create(consumer_key, consumer_secret)
+    {:ok, client: client}
+  end
+
+  test "#get_request_token/1 returns the consumers tokens", %{client: client} do
     use_cassette "request_token" do
-      {consumer, tokens} = Twitter.request_token(consumer_key, consumer_secret)
-      assert(consumer == consumer)
+      {:ok, tokens} = Twitter.get_request_tokens(client)
       assert(tokens == consumer_credentials)
     end
   end
 
-  test "#client_access_token/3 get client credentials using a consumer, consumer_credentials & verification code" do
+  test "#get_access_token/2 returns client tokens", %{client: client} do
     use_cassette "client_access_token" do
-      tokens = Twitter.client_access_token(consumer, consumer_credentials, verifier)
+      Twitter.set_request_tokens(client, consumer_token, consumer_secret)
+      {:ok, tokens} = Twitter.get_access_tokens(client, verifier)
       assert(tokens == client_credentials)
     end
   end
 
-  test "#get/4 returns the decoded JSON body of the response" do
+  test "#get/2 returns the decoded JSON body of the response", %{client: client} do
     use_cassette "verify_token" do
-      result = Twitter.get('/account/verify_credentials.json', [], consumer, client_credentials)
+      Twitter.set_access_tokens(client, client_token, client_token_secret)
+      {:ok, result} = Twitter.get(client, '/account/verify_credentials.json')
       assert(97178399 == result["id"])
     end
   end
@@ -36,12 +42,11 @@ defmodule TwitterTest do
   defp consumer_token_secret, do: 'tTJQt5BshROmAjVKm2gNfuWHCNpJVMoK'
 
   defp consumer_credentials, do: {consumer_token, consumer_token_secret}
-  defp consumer, do: {consumer_key, consumer_secret, :hmac_sha1}
+
+  defp verifier, do: 'tCiVGwgbaKgW7DxCEiYOTUe65qlDAd3p'
 
   defp client_token, do: '97178399-UKyCuDOmHeXBVn6SDtTipt0EuVC8LSPrgO3igOqEy'
   defp client_token_secret, do: '38POBYgidpMkKJlTv1HJKSQp3vDKv1JWjKXii4IHa2Adw'
-
-  defp verifier, do: 'tCiVGwgbaKgW7DxCEiYOTUe65qlDAd3p'
 
   defp client_credentials, do: {client_token, client_token_secret}
 end
